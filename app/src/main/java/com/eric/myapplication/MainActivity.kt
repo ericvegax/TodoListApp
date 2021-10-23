385d2e31debdec84206080ab7ce34e88731d61f1
Eric <65141753+realericvega@users.noreply.github.com>
2021-10-23 05:37:18 -0400

    Update MainActivity.kt

--- app/src/main/java/com/eric/myapplication/MainActivity.kt
+++ app/src/main/java/com/eric/myapplication/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter : TodoAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) { // Called once this Activity is created
+    override fun onCreate(savedInstanceState: Bundle?) { 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
